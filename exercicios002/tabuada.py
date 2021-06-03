3a7729fc72b285fa8221e3f25db8e9ade15e7ba6
Tiago Oliveira <67693562+Tiagoksio@users.noreply.github.com>
2021-06-02 23:41:37 -0300

    add tabuada

---
+++ exercicios002/tabuada.py
@@ -0,0 +1,5 @@
+num = int(input('Informe o número que deseja saber a tabuada: '))
+multiplica = 0
+while  multiplica <= 10:
+    print('{} x {} = {}'.format(num, multiplica, num * multiplica))
+    multiplica += 1
