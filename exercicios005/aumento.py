4db06aa334e61ed737ad921859594e77a52379a8
Tiago Oliveira <67693562+Tiagoksio@users.noreply.github.com>
2021-06-05 09:58:05 -0300

    Create aumento.py

---
+++ exercicios005/aumento.py
@@ -0,0 +1,3 @@
+# Escreva um programa que pergunte o salário de um funcionário e calcule o valor do seu aumento. Para salários superiores à **R$1250,00**, calcule um aumento de **10%**. Para os inferiores ou iguais, o aumento é de **15%**;
+salario = float(input('Informe o valor do salário: '))
+print("O salário de R${:.2f} receberá um aumento de 10%, e ficará R${:.2f}".format(salario, salario + salario * 0.1) if salario >= 1250 else "O salário de R${:.2f} receberá um aumento de 15%, e ficará R${:.2f}".format(salario, salario + salario * 0.15))
