4a39f86d2664c59bec8c15de6a9a0bf833191c83
Tiago Oliveira <67693562+Tiagoksio@users.noreply.github.com>
2021-06-05 04:59:35 -0300

    Update analiseNome.py

--- exercicios004/analiseNome.py
+++ exercicios004/analiseNome.py
@@ -1,5 +1,5 @@
-nome = input('Informe o nome completo para análise: ')
+nome = " ".join(input('Informe o nome completo para análise: ').upper().split())
 print("""Nome com letras maiúsculas: {};
 Nº de letras sem os espaços: {}
 Nº de letras do primeiro nome: {}
-Primeiro e o último nome: {}, {}""".format(nome.upper(), len(nome) - len(nome.split()) + 1, len(nome.split()[0]), nome.split()[0], nome.split()[len(nome.split()) - 1]))
+Primeiro e o último nome: {}, {}""".format(nome.upper(), len(nome) - len(nome.split()) + 1, len(nome.split()[0]), nome.split()[0].capitalize(), nome.split()[len(nome.split()) - 1].capitalize()))
