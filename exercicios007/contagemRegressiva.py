34231b9cbab3a5cb271bdaade3f4a310c951d395
Tiago Oliveira <67693562+Tiagoksio@users.noreply.github.com>
2021-06-08 06:07:20 -0300

    Create contagemRegressiva.py

---
+++ exercicios007/contagemRegressiva.py
@@ -0,0 +1,14 @@
+# Faça um programa que mostre na tela uma contagem regressiva para o estouro de fogos de artifício, indo de 10 até 0, com uma pausa de 1 segundo entre eles;
+from time import sleep
+from pygame import mixer
+
+iniciar = input('Deseja iniciar? [s/n]').lower()
+if iniciar == 's':
+    for contando in range (10, -1, -1):
+        print('{}...'.format(contando))
+        sleep(0.5)
+    print('FELIZ ANO NOVO!!!')
+    mixer.init()
+    musica = mixer.Sound('/home/tksio/Documentos/Python/exercicios007/fireworks.ogg')
+    musica.play(-1)
+    parar = input('APERTE QUALQUER TECLA PARA SAIR...')
