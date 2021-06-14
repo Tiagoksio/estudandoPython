c0abcca1a386dd0770ef5a4bc604c1b493e182cf
Tiago Oliveira <67693562+Tiagoksio@users.noreply.github.com>
2021-06-14 14:54:57 -0300

    Create inclusaoDeNumEmTupla.py

---
+++ exercicios010/inclusaoDeNumEmTupla.py
@@ -0,0 +1,4 @@
+# Crie um programa que vai gerar cinco números aleatórios e colocar em uma tupla. Depois disso, mostre a listagem de números gerados e também indique o menor e o maior valor que estão na tupla;
+from random import randint
+num = (randint(1,10), randint(1,10), randint(1,10), randint(1,10), randint(1,10))
+print(f'Os valores sorteados foram {num}\nO maior foi {max(num)}\nO menor foi {min(num)}')
